892fdd932d60a1b59f6afef79d3a15411549f6f6
Rishabh Raj <rajrishabh2002711@gamil.com>
2025-09-12 20:35:50 +0530

    add text display

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,100 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List<StringBuilder> page = new ArrayList<>();
+        page.add(new StringBuilder());
+        int[] cursor = {0, 0};
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.println("\033[2J");
+            System.out.println("Cursor:" + Arrays.toString(cursor));
+            System.out.println();
+
+            // Document display
+            for (int i = 0; i < page.size(); i++){
+                System.out.print(i + " ");
+                if (i == cursor[0]){
+                    String line = new String(page.get(i));
+                    String before = line.substring(0, cursor[1]);
+                    char cursorChar = cursor[1] < line.length() ? line.charAt(cursor[1]) : ' ';
+                    String after = cursor[1] < line.length() ? line.substring(cursor[1] + 1, line.length()) : "";
+
+                    // Display highlighted Cursor
+                    System.out.print(before);
+                    System.out.print("[" + cursorChar + "]");
+                    System.out.print(after + "\n");
+                } else {
+                    System.out.print(page.get(i) + "\n");
+                }
+            }
+
+
+            // Handle Command prompt tokens
+            System.out.print("\n\n> ");
+            String[] tokens = br.readLine().split(" ");
+            String command = tokens[0];
+            int lineNum = cursor[0];
+            String content = "";
+
+            // Handle Commands
+            switch (command) {
+                case "append", "a":
+                    content = String.join(" ", Arrays.copyOfRange(tokens, 1, tokens.length));
+                    page.get(cursor[0]).append(content);
+                    cursor[1] += content.length();
+                    break;
+
+                case "insert", "i":
+                    content = String.join(" ", Arrays.copyOfRange(tokens, 2, tokens.length));
+                    lineNum = Integer.parseInt(tokens[1]);
+                    while (page.size() < lineNum + 1){
+                        page.add(new StringBuilder());
+                    }
+                    page.get(lineNum).append(content);
+                    cursor[0] = lineNum;
+                    cursor[1] = page.get(lineNum).length();
+                    break;
+
+                case "delete", "del":
+                    lineNum = Integer.parseInt(tokens[1]);
+                    page.remove(lineNum);
+                    cursor[0] = cursor[0] == 0 ? 0 : cursor[0] - 1;
+                    cursor[1] = page.get(cursor[0]).length();
+                    break;
+
+                case "addNewLine", "nl":
+                    page.add(cursor[0] + 1, new StringBuilder());
+                    cursor[0]++;
+                    cursor[1] = 0;
+                    break;
+
+                case "left":
+                    cursor[1]--;
+                    break;
+                case "right":
+                    cursor[1]++;
+                    break;
+
+                case "quit", "q":
+                    return;
+
+                default:
+                    System.out.println("invlaid command! " + command);
+                    break;
+            }
+            fixCursor(page, cursor);
         }
+    }
+
+    public static void fixCursor(List<StringBuilder> page, int[] cursor){
+        cursor[0] = Math.max(0, Math.min(cursor[0], page.size() - 1));
+        cursor[1] = Math.max(0, Math.min(cursor[1], page.get(cursor[0]).length()));
     }
 }
